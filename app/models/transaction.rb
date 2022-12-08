cedf19783d49edc0a384057e3407d4aa191d040e
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-12-08 17:08:22 -0300

    Config validacoes do model  transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :account
-
   enum :kind, { 'deposit' => 0, 'withdraw' => 1 }
+
+  validates :kind, inclusion: { in: %w[deposit withdraw] }
+  validates :account, presence: true
+  validates :value, presence: true, numericality: true
 end
