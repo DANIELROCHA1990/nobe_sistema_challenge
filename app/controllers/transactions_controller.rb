585eee8c450c78721c1555234d962cf36d55c58e
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-12-08 17:06:57 -0300

    Cria transactions controller

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,9 @@
+class TransactionsController < ApplicationController
+  def withdraw(value); end
+
+  def deposit(value); end
+
+  def transf(value, account); end
+
+  def extract(start_date, end_date); end
+end
