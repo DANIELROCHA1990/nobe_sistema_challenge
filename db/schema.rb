6f6607558fe69c8e97499ce19ac4602c3ef190bb
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-12-08 17:12:02 -0300

    cria /config migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_205159) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_08_180928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_205159) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.boolean "state"
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
