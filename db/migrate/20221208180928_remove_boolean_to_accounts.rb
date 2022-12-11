4ab295abdb91a0b8081fa34323dd6e09c57015ce
Daniel_Rocha <daniel1990rocha@gmail.com>
2022-12-11 14:37:46 -0300

    fix bugs

--- db/migrate/20221208180928_remove_boolean_to_accounts.rb
+++ db/migrate/20221208180928_remove_boolean_to_accounts.rb
@@ -1,5 +1,5 @@
 class RemoveBooleanToAccounts < ActiveRecord::Migration[7.0]
   def change
-    remove_column :accounts, :boolean, :string
+    # remove_column :accounts, :boolean, :string
   end
 end
